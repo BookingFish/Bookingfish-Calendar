54a4cbb595708de634a8b41c813861a5660d0f1f
BookingFish <support@bookingfish.ca>
2026-04-27 18:02:29 -0400

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -8,8 +8,18 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
 
-$options = array(
-    // Current option names (v1.2.3+)
+$bfish_options = array(
+    // Current option names (v1.2.14+)
+    'bfish_language',
+    'bfish_auth_token',
+    'bfish_token_expires',
+    'bfish_vendor_email',
+    'bfish_vendor_name',
+    'bfish_embed_codes',
+    'bfish_created_pages',
+    'bfish_last_sync',
+    'bfish_last_login_email',
+    // Previous option names (v1.2.3–1.2.13)
     'bfc_language',
     'bfc_auth_token',
     'bfc_token_expires',
@@ -30,10 +40,11 @@ $options = array(
     'bfcc_last_sync',
 );
 
-foreach ( $options as $option ) {
-    delete_option( $option );
+foreach ( $bfish_options as $bfish_option ) {
+    delete_option( $bfish_option );
 }
 
 // Remove scheduled cron events
+wp_clear_scheduled_hook( 'bfish_daily_sync' );
 wp_clear_scheduled_hook( 'bfc_daily_sync' );
 wp_clear_scheduled_hook( 'bfcc_daily_sync' );
